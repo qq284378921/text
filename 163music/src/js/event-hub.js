43d49f53d533e005d208ebc31792bc4292de1fdd
jimmy <qq284378921@163.com>
2019-04-20 14:06:46 +0800

    2019-4-20

--- 163music/src/js/event-hub.js
+++ 163music/src/js/event-hub.js
@@ -1,19 +1,31 @@
 window.eventHub = {
     events: {
-        '羊城晚报': [],
-        '深圳晚报': []
-    },
-    emit(){
+      //'羊城晚报': [fn, fn2, fn3],
+      //'楚天都市报': [],
+    }, // hash
+    emit(eventName, data){ //发布
+    //     this.events[eventName][0].call(undefined, data)
+    //     fn(data)
+    //     if(this.events[eventName] !== undefined) {
+            
+    //     }
 
-    },
-    on(eventName, fn){
-        for(let key in this.events){
-            if(key === eventName){
-                this.events[key].push(fn)
-            }
+      for(let key in this.events){
+        if(key === eventName){
+          let fnList = this.events[key]
+          fnList.map((fn)=>{
+            fn.call(undefined, data)
+          })
         }
+      }
     },
-    off(){
-
+    on(eventName, fn){ //订阅
+      if(this.events[eventName] === undefined){
+        this.events[eventName] = []
+      }
+      this.events[eventName].push(fn)
     },
-}
+  }
+  function fn() {
+      console.log('nifasong')
+  }
